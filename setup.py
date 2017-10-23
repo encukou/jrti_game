e50c77d7672b8421c926f43e84eb861e1207f19b
Petr Viktorin <encukou@gmail.com>
2017-10-23 10:55:05 +0200

    Add a setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name='jrti_game',
+    version='1.0',
+    description='Just Read the Instructions, the game',
+    author='Petr Viktorin',
+    author_email='encukou@gmail.com',
+    license='MIT',
+    url='https://pyweek.org/e/instructions/',
+    packages=['jrti_game'],
+    install_requires=[
+        'pyglet == 1.2.4',
+        'pypng == 0.0.18',
+        'attrs == 17.2.0',
+    ],
+    entry_points={
+        'console_scripts': [
+            'jrti-game = jrti_game:main',
+        ],
+    },
+)
